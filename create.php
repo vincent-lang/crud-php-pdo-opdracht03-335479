bc156a66a34729966dbbafbc069a8bc00a7a8d7b
vincent van de merwe <335479@student.mboutrecht.nl>
2023-02-09 20:55:58 +0100

    sixth commit

--- create.php
+++ create.php
@@ -14,12 +14,30 @@ try {
 }
 
 $sql = "INSERT INTO achtbaan (Id
-                        ,naam_achtbaan)
+                        ,naam_achtbaan
+                        ,naam_pretpark
+                        ,naam_land
+                        ,topsnelheid
+                        ,hoogte
+                        ,datum
+                        ,cijfer)
             VALUES      (NULL
-                        ,:naam_achtbaan);";
+                        ,:naam_achtbaan
+                        ,:naam_pretpark
+                        ,:naam_land
+                        ,:topsnelheid
+                        ,:hoogte
+                        ,:datum
+                        ,:cijfer);";
 $statement = $pdo->prepare($sql);
 
 $statement->bindValue(":naam_achtbaan", $_POST["naam_achtbaan"], PDO::PARAM_STR);
+$statement->bindValue(":naam_pretpark", $_POST["naam_pretpark"], PDO::PARAM_STR);
+$statement->bindValue(":naam_land", $_POST["naam_land"], PDO::PARAM_STR);
+$statement->bindValue(":topsnelheid", $_POST["topsnelheid"], PDO::PARAM_STR);
+$statement->bindValue(":hoogte", $_POST["hoogte"], PDO::PARAM_STR);
+$statement->bindValue(":datum", $_POST["datum"], PDO::PARAM_STR);
+$statement->bindValue(":cijfer", $_POST["cijfer"], PDO::PARAM_STR);
 
 $statement->execute();
 
