71c4c90d8a21a869053cdeddf98c8b8e136e6f73
vincent van de merwe <335479@student.mboutrecht.nl>
2023-02-09 20:32:23 +0100

    third commit

--- index.php
+++ index.php
@@ -24,9 +24,26 @@
             <br>
             <input type="text" name="naam_land" id="naam_land">
             <br>
+            <label for="topsnelheid">Topsnelheid (km/u):</label>
+            <br>
+            <input type="number" name="topsnelheid" id="topsnelheid" min="1" max="200">
+            <br>
+            <label for="hoogte">Hoogte (m):</label>
+            <br>
+            <input type="number" name="hoogte" id="hoogte" min="1" max="200">
+            <br>
+            <label for="datum">Datum eerste opening:</label>
+            <br>
+            <input type="date" name="datum" id="datum">
+            <br>
+            <label for="cijfer">Cijfer voor achtbaan:</label>
+            <br>
+            <input type="range" name="cijfer" id="cijfer" min="1" max="10" step="0.1" oninput="num.value = this.value">
+            <output id="num">5.5</output>
+            <br>
             <input id="submit" type="submit" value="Sla op">
         </fieldset>
     </form>
-    </form>
+    <script src="js/test.js"></script>
 </body>
 </html>
